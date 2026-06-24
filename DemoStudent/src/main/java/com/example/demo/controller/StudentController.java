022e5933e0c6252ebfa80265ff83f4be71b4d113
Arpita Patil <ap2972004@email.com>
2026-06-24 22:52:06 +0530

    stud

--- DemoStudent/src/main/java/com/example/demo/controller/StudentController.java
+++ DemoStudent/src/main/java/com/example/demo/controller/StudentController.java
@@ -3,8 +3,12 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Student;
@@ -15,14 +19,30 @@ public class StudentController {
 
 	@Autowired
 	private StudentService ss;
-	
+
 	@GetMapping("display")
-	public List<Student> displayStudents(){
+	public List<Student> displayStudents() {
 		return ss.display();
 	}
-	
+
 	@PostMapping("add")
 	public void addStudent(Student s) {
 		ss.add(s);
 	}
+
+	@DeleteMapping("delete/{Id}")
+	public void deleteStudent(@PathVariable("Id") int roll) {
+		ss.delete(roll);
+	}
+
+	@PutMapping("update/{existingId}")
+	public Student updateStudent(@PathVariable("existingId") int id, @RequestBody Student s) {
+		return ss.update(id, s);
+	}
+
+	@PostMapping("addAll")
+	public void addAllStudent(@RequestBody List<Student> s) {
+		ss.addAll(s);
+	}
+
 }
