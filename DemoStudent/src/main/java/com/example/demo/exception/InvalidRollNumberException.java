543d7f1d387553a822255454ee94c4f37167131a
Arpita Patil <ap2972004@email.com>
2026-06-25 13:49:33 +0530

    Commit

--- DemoStudent/src/main/java/com/example/demo/exception/InvalidRollNumberException.java
+++ DemoStudent/src/main/java/com/example/demo/exception/InvalidRollNumberException.java
@@ -1,7 +1,7 @@
 package com.example.demo.exception;
 
 public class InvalidRollNumberException extends RuntimeException {
-public InvalidRollNumberException(String msg) {
-	super(msg);
-}
+	public InvalidRollNumberException(String msg) {
+		super(msg);
+	}
 }
